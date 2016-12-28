e8ec5574493f2b26613a0cfc5c129e0f9dc09397
mark <wangjunfei@uml-tech.com>
2016-12-28 17:02:09 +0800

    no message

--- PanelViewShare/PanelViewShare/PanelView.swift
+++ PanelViewShare/PanelViewShare/PanelView.swift
@@ -1,26 +1,55 @@
 //
-//  ViewController.swift
-//  PanelViewShare
+//  PanelView.swift
+//  WeiBoTest
 //
 //  Created by apple on 28/12/16.
 //  Copyright © 2016年 mark. All rights reserved.
-//
+//  封装的版本
+/*
+  使用方法:
+  外部创建
+   lazy var panel:PanelView = PanelView()
 
+   //设置视图位置，添加到view
+   panel.center = view.center
+ 
+   view.addSubview(panel)
+ 
+
+*/
 import UIKit
 
-class ViewController: UIViewController {
+class PanelView: UIView {
 
-    var timer: Timer?
-    lazy var progressLayer = CAShapeLayer()
     
+    public lazy var progressLayer = CAShapeLayer()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        /// 创建圆
+        createCircle()
+        
+        /// 创建刻度
+        createscale()
+        
+        /// 创建刻度值
+        createCircleValue()
+        
+        /// 进度曲线
+        createProgressLine()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     
     /// 创建圆
     fileprivate func createCircle(){
         
         /// 创建圆
-        let cicrle = UIBezierPath(arcCenter: view.center,
+        let cicrle = UIBezierPath(arcCenter: center,
                                   radius: 100,
                                   startAngle: -(CGFloat(M_PI*9)/8),
                                   endAngle: (CGFloat(M_PI)/8),
@@ -32,7 +61,7 @@ class ViewController: UIViewController {
         shapeLayer.strokeColor = UIColor(colorLiteralRed: 185/255.0, green: 243/255.0, blue: 110/255.0, alpha: 1.0).cgColor
         shapeLayer.path = cicrle.cgPath
         
-        self.view.layer.addSublayer(shapeLayer)
+        self.layer.addSublayer(shapeLayer)
         
         
     }
@@ -49,7 +78,7 @@ class ViewController: UIViewController {
             let startAngle = -(CGFloat(M_PI*9)/8) + perAngle*CGFloat(i)
             let endAngle = startAngle + perAngle/5
             
-            let tickPath = UIBezierPath(arcCenter: view.center,
+            let tickPath = UIBezierPath(arcCenter: center,
                                         radius: 140,
                                         startAngle: startAngle,
                                         endAngle: endAngle,
@@ -69,7 +98,7 @@ class ViewController: UIViewController {
             
             perLayer.path = tickPath.cgPath
             
-            self.view.layer.addSublayer(perLayer)
+            self.layer.addSublayer(perLayer)
         }
         
     }
@@ -83,7 +112,7 @@ class ViewController: UIViewController {
         
         for i in 0...10 {
             
-            let point = calculateTextPositon(view.center,-Float(M_PI)/8+textAngle*Float(i))
+            let point = calculateTextPositon(center,-Float(M_PI)/8+textAngle*Float(i))
             print(point)
             let tickString = "\(labs(10-i)*10)"
             
@@ -92,7 +121,7 @@ class ViewController: UIViewController {
             label.font = UIFont.systemFont(ofSize: 10)
             label.textColor = UIColor.gray
             label.textAlignment = .center
-            view.addSubview(label)
+            addSubview(label)
         }
     }
     
@@ -100,7 +129,7 @@ class ViewController: UIViewController {
     fileprivate func createProgressLine(){
         
         /// 进度曲线
-        let progressPath = UIBezierPath(arcCenter: view.center,
+        let progressPath = UIBezierPath(arcCenter: center,
                                         radius: 120,
                                         startAngle: -(CGFloat(M_PI*9)/8),
                                         endAngle: (CGFloat(M_PI)/8),
@@ -111,37 +140,9 @@ class ViewController: UIViewController {
         progressLayer.path = progressPath.cgPath
         progressLayer.strokeStart = 0
         progressLayer.strokeEnd = 0.5
-        view.layer.addSublayer(progressLayer)
+        layer.addSublayer(progressLayer)
         
     }
-    
-    lazy var panel:PanelView = PanelView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-         //封装的版本
-        
-//        panel.center = view.center
-//        
-//        view.addSubview(panel)
-        
-        
-        /// 创建圆
-        createCircle()
-        
-        /// 创建刻度
-        createscale()
-        
-        /// 创建刻度值
-        createCircleValue()
-        
-        /// 进度曲线
-        createProgressLine()
-        
-        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(timesTest), userInfo: nil, repeats: true)
-    }
-    
     
     /// 计算文本的位置
     private func calculateTextPositon(_ ArcCenter: CGPoint,_ angle: Float) -> CGPoint{
@@ -151,17 +152,6 @@ class ViewController: UIViewController {
         
         return CGPoint(x: ArcCenter.x + CGFloat(x), y: ArcCenter.y - CGFloat(y))
     }
-    
-    @objc func timesTest(){
-        
-        let a = CGFloat(arc4random()%100)
-        
-        UIView.animate(withDuration: 0.3) {
-            self.progressLayer.strokeEnd = a*0.01
-        }
-    }
-
 
 
 }
-
